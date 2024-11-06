91ce72b4d98a3be4fed7d4a7af45898ee7c7ac3e
MorningRocks <474489174@qq.com>
2024-11-06 12:55:56 +0800

    1

---
+++ App_Test/Application_Cli_Test.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+import pytest
+
+from rl_interaction import Run_Test
+
+
+@pytest.fixture(scope="session")
+def valid_apk_path():
+    return os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "test_resources",
+        "InsecureBankv2.apk",
+    )
+
+
+
+class TestCommandLine(object):
+    def test_help_message(self, monkeypatch):
+        # Mock the command line parser.
+        with monkeypatch.context() as m:
+            m.setattr(sys, "argv", ["program", "--help"])
+            with pytest.raises(SystemExit) as e:
+                Run_Test.main()
+            assert e.value.code == 0
+
+    def test_missing_required_parameters(self, monkeypatch):
+        # Mock the command line parser.
+        with monkeypatch.context() as m:
+            m.setattr(sys, "argv", ["program"])
+            with pytest.raises(SystemExit) as e:
+                Run_Test.main()
+            assert e.value.code == 2
+
+    def test_real_application(self, monkeypatch, valid_apk_path):
+        if (
+            os.environ.get("GITHUB_ACTIONS", "false").lower() == "true"
+            and os.environ.get("RUNNER_OS", "unknown") == "macOS"
+        ):
+            # Run this test only from GitHub Actions, when running on macOS
+            # (since is the only OS providing hardware accelerated Android emulators).
+            pass
+
+            # Mock the command line parser.
+            with monkeypatch.context() as m:
+                m.setattr(
+                    sys,
+                    "argv",
+                    [
+                        "program",
+                        "--device_name",
+                        "test",  # Emulator name.
+                        "--algo",
+                        "SAC",
+                        "--appium_port",
+                        "4270",
+                        "--timer",
+                        "1",  # The test will stop after 1 minute.
+                        "--iterations",
+                        "1",
+                        "--pool_strings",
+                        os.path.join(
+                            os.path.dirname(os.path.realpath(__file__)),
+                            os.path.pardir,
+                            "rl_interaction",
+                            "strings.txt",
+                        ),
+                        "--apps",
+                        valid_apk_path,
+                    ],
+                )
+                exit_code = Run_Test.main()
+                assert exit_code == 0
---
+++ App_Test/Tools.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+from rl_interaction.utils.utils import AppiumLauncher, Utils
+
+
+class TestAppium(object):
+    def test_appium_valid_command(self):
+        appium_path = Utils.get_appium_executable_path()
+        assert os.path.isfile(appium_path)
+        output = subprocess.check_output(
+            [appium_path, "--help"], stderr=subprocess.STDOUT
+        ).decode()
+        assert "show this help message and exit" in output.lower()
+
+    def test_appium_launcher(self):
+        appium = AppiumLauncher(4270)
+        appium.restart_appium()
+        appium.terminate()
+        
+
+
+class TestAdb(object):
+    def test_adb_valid_command(self):
+        adb_path = Utils.get_adb_executable_path()
+        assert os.path.isfile(adb_path)
+        output = subprocess.check_output(
+            [adb_path, "--help"], stderr=subprocess.STDOUT
+        ).decode()
+        assert "show this help message" in output.lower()
+
+
+class TestEmulator(object):
+    def test_emulator_valid_command(self):
+        emulator_path = Utils.get_emulator_executable_path()
+        assert os.path.isfile(emulator_path)
+        output = subprocess.check_output(
+            [emulator_path, "-help"], stderr=subprocess.STDOUT
+        ).decode()
+        assert "print this help" in output.lower()
